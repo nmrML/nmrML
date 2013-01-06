8805502db91a474678032d3279d5a70bed842b8e
Steffen Neumann <sneumann@ipb-halle.de>
2013-01-06 22:20:24 +0100

    modfiy path after re-organisation

--- R/nmrml.R
+++ R/nmrml.R
@@ -1,9 +1,9 @@
 library(XML)
 library(ontoCAT)
 
-msiNMR <- getOntology("xsd+obo/msi-nmr.obo")
+msiNMR <- getOntology("../xsd+obo/msi-nmr.obo")
 unit <- getOntology("http://unit-ontology.googlecode.com/svn/trunk/unit.obo")
 
-tree <-  xmlTreeParse("examples/biosample-concentrations.xml")
+tree <-  xmlTreeParse("../examples/biosample-concentrations.xml")
 root <-  xmlRoot(tree)
 
