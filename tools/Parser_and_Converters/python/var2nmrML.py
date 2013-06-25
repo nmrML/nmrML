587ca13937ae7fb9ac15e1ce9771075cd8761779
Michael Wilson <michael.wilson@ualberta.ca>
2013-06-25 13:37:11 +0200

    adding real sample file

--- tools/Parser_and_Converters/python/var2nmrML.py
+++ tools/Parser_and_Converters/python/var2nmrML.py
@@ -1,7 +1,54 @@
+#!/usr/bin/env python
+import sys
 import nmrglue
-from lxml import etree
+#from lxml import etree
 
-dic,data = nmrglue.varian.read("sucrosesample_varian/")
+filename = str(sys.argv[1])
+
+dic,data = nmrglue.varian.read(filename)
+procpar  = dic["procpar"]
+
+#print(procpar.keys())
+print( "number of scans",  procpar["nt"]['values'][0] )
+
+print( "number of steady state scans",  procpar["ss"]['values'][0] )
+
+print( "relaxation delay", procpar["d1"]['values'][0] )
+print( "spectral width",  procpar["sw"]['values'][0] )
+print("transmitter offset", procpar["tof"]["values"][0])
+print("transmitter power", procpar["tpwr"]["values"][0])
+print("solvent parameter", procpar["solvent"]["values"][0])
+print("acquisition time", procpar["at"]["values"][0])
+print("number of data points", procpar["np"]["values"][0])
+print("nucleus being detected", procpar["tn"]["values"][0])
+# in MHz
+print("spectrometer frequency", procpar["sfrq"]["values"][0]) 
+
+# TODO may not work without vttype, look into this more
+print("probe/sample temperature", procpar["temp"]["values"][0]) 
+
+print("spin rate", procpar["spin"]["values"][0]) 
+print("decoupler offset", procpar["dof"]["values"][0]) 
+print("decoupler nucleus", procpar["dn"]["values"][0]) 
+# In MHz:
+print("decoupler frequency", procpar["dfrq"]["values"][0]) 
+# decoupler mode; sets when the decoupler is turned on
+#for example, dm = nnn means 
+# decoupler is set to no for the whole experiment; dm = 'nny' means decoupler is on during 
+# timer period 3, usually the acquisition time; dm = 'ynn' means decoupler is on during 
+# time period 1, normally the d1 relaxation delay
+print("decoupler mode", procpar["dm"]["values"][0]) 
+print("decoupler modulation mode", procpar["dmm"]["values"][0]) 
+print("decoupler power", procpar["dpwr"]["values"][0]) 
+print("decoupler modulation frequency", procpar["dmf"]["values"][0]) 
+print("2nd decoupler nucleus frequency", procpar["dfrq2"]["values"][0]) 
+# Then need: dmf2, dpwr2, dmm2, dm2 
+
+# values we should consider including?
+# gain
+
+
+exit()
 
 root = etree.Element("nmrML")
 
