599e8a8676865fd8c43cd8c0ad60f41e5eac0145
Steffen Neumann <sneumann@ipb-halle.de>
2013-07-11 11:40:26 +0200

    Updated R reader with examples of OWL Ontology reading, updated to read the nmrML XSD.

--- tools/Parser_and_Converters/R/reader.R
+++ tools/Parser_and_Converters/R/reader.R
@@ -16,13 +16,35 @@
 # 	source("tools/R/reader.R")
 # 
 library(XML)
-library(ontoCAT)
 
-# Load ontologies
-msiNMR <- getOntology("ontologies/msi-nmr.obo")
+## Load ontologies in OBO format,
+## Likely to be deprecated in favour of OWL
+library(ontoCAT)
+msiNMR <- getOntology("../../../ontologies/msi-nmr.obo")
 unit   <- getOntology("http://unit-ontology.googlecode.com/svn/trunk/unit.obo")
+
+
+
+##
+## Ontology in OWL
+##
+## nmrML is currently not clean enough for ontoCAT:
+nmrMLOnto <- getOntology("../../../ontologies/nmrMLv.9.owl")
+
+#btl2Onto <- getOntology("http://purl.org/biotop/btl2.owl")
+btl2Onto <- getOntology("https://biotop.googlecode.com/svn/trunk/btl2.owl")
+
+##
+## XSD Schema
+##
+library(XMLSchema)
+d = readSchema("../../../xml-schemata/nmrML.xsd")
+## I am not yet sure how to use the powerful XMLSchema package,
+## but here is the "Contact Type":
+str(d@.Data[[1]]@.Data[[6]], max.level=2)
 
 # Parse an XML sample file
 tree <- xmlTreeParse("../../../examples/nmrML/biosample-concentrations.xml")
 root <- xmlRoot(tree)
+
 
