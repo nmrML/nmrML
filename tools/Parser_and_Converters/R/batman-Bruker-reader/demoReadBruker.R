d7e80e22dcb1d6f14cf0c6b9d3b39410eb4a1f6a
jieh <j.hao@hotmail.co.uk>
2013-07-17 11:03:28 +0100

    added comment

--- tools/Parser_and_Converters/R/batman-Bruker-reader/demoReadBruker.R
+++ tools/Parser_and_Converters/R/batman-Bruker-reader/demoReadBruker.R
@@ -1,11 +1,14 @@
-
 source('~/readBrukerZipped.R')
 source('~/readBruker.R')
-# Read in multiple raw binary Bruker NMR spectra (1D) from a specified folder, 
-# and return a matrix with columns:
+# Read in multiple raw binary Bruker NMR spectra (1D) from a specified directory, 
+# all the 1r files in that directory will be read in, the title files will also be read in, 
+# and assigned as the column name to the corresponding specctrum column.
+# The returned matrix has columns as the following format:
 #         [ppm, spectrum1, spectrum2, ...]. 
 # Interpolation may be performed if spectra have different ppm scales.
+# BrukerDataDir: directory for bruker data files.
+# BrukerDataZipDir: directory for zipped bruker data files.
 sa<-readBruker(BrukerDataDir)  
-# or if zipped Bruker files, example files from: http://www.ebi.ac.uk/metabolights/MTBLS1
+# or if zipped Bruker files, to read in example dataset in MTBLS1
 sa<-readBrukerZipped(BrukerDataZipDir)
 
