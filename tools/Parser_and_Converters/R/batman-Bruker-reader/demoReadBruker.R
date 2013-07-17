99cf8a1fac6af5c7874554122535145484b442c0
Steffen Neumann <sneumann@ipb-halle.de>
2013-07-17 18:53:33 +0200

    Use filenames realtiv to the script directory

--- tools/Parser_and_Converters/R/batman-Bruker-reader/demoReadBruker.R
+++ tools/Parser_and_Converters/R/batman-Bruker-reader/demoReadBruker.R
@@ -1,5 +1,5 @@
-source('~/readBrukerZipped.R')
-source('~/readBruker.R')
+source('./readBrukerZipped.R')
+source('./readBruker.R')
 # Read in multiple raw binary Bruker NMR spectra (1D) from a specified directory, 
 # all the 1r files in that directory will be read in, the title files will also be read in, 
 # and assigned as the column name to the corresponding specctrum column.
@@ -8,7 +8,11 @@ source('~/readBruker.R')
 # Interpolation may be performed if spectra have different ppm scales.
 # BrukerDataDir: directory for bruker data files.
 # BrukerDataZipDir: directory for zipped bruker data files.
-sa<-readBruker(BrukerDataDir)  
+
+
+# sa<-readBruker(BrukerDataDir)  
 # or if zipped Bruker files, to read in example dataset in MTBLS1
-sa<-readBrukerZipped(BrukerDataZipDir)
+
+BrukerDataZipDir <- "../../../../examples/MTBLS1_DiagramUseCase/MTBLS1"
+sa <- readBrukerZipped(BrukerDataZipDir)
 
