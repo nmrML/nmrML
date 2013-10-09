d36ed49dec211309a07b418b054ded4131814cad
Steffen Neumann <sneumann@ipb-halle.de>
2013-10-09 23:46:10 +0200

    Package nmRIO survives R CMD check, few Warnings left

--- tools/Parser_and_Converters/R/nmRIO/R/readNMRMLFID.R
+++ tools/Parser_and_Converters/R/nmRIO/R/readNMRMLFID.R
@@ -1,30 +1,29 @@
-readNMRMLFID <-
-function (filename) {
+#' readNMRMLFID
+#'
+#' Extract binary FID data from nmrML
+#'
+#' This is the Details section
+#'
+#' @param filename character Filename of the nmrML to check
+#' @return A vector with the complex values of the FID data
+#' @author Steffen Neumann
+#' @examples
+#' length(readNMRMLFID(system.file("examples/HMDB00005.nmrML", package = "nmRIO")))
+#' @export
+
+readNMRMLFID <- function (filename) {
     tree <- xmlTreeParse(filename)
     root <- xmlRoot(tree)
 
     ## Extract base64encoded data 
-    b64s <- gsub("\n", "", sapply (xmlElementsByTagName(root, "binary",
-                                         recursive = TRUE), xmlValue))
+    b64s <- gsub("\n", "", sapply (xmlElementsByTagName(root, "binary", recursive = TRUE),
+                                   xmlValue))
+
+    ## Decode. TODO: Check cvParam about the encoding
     rfid <- memDecompress(base64decode(b64s["acquisition.acquisition1D.fid.binary"], "raw"), type="gzip")
     dfid <- readBin(rfid, n=length(rfid)/4+1, what="double", size=4)
     fid <- complex(real=dfid[c(TRUE,FALSE)], imaginary=dfid[c(FALSE,TRUE)])
-    
-    ## Get required parameters from nmrML
-    irradiationFrequency <- as.double(xmlAttrs(xmlElementsByTagName(root, "irradiationFrequency", recursive = TRUE)[[1]])["value"])
 
-    sweepWidth <- as.double(xmlAttrs(xmlElementsByTagName(root, "sweepWidth", recursive = TRUE)[[1]])["value"])
-
-    numberOfDataPoints <- as.integer(xmlAttrs(xmlElementsByTagName(root, "DirectDimensionParameterSet", recursive = TRUE)[[1]])["numberOfDataPoints"])
-
-    ## delayTime <- 1.0
-    ## ppmOffset <- delayTime / 599.4094446,
-    
-    ppm <- seq(from=14.77180,
-               to= -5.239921,
-               length=numberOfDataPoints)
-        
-    datamatrix <- cbind(ppm, intensities)
-    names(datamatrix) <- c("ppm", basename(filename))
-    datamatrix
+    ## Return the fid
+    fid
 }
--- tools/Parser_and_Converters/R/nmRIO/R/readNMRMLProcessed.R
+++ tools/Parser_and_Converters/R/nmRIO/R/readNMRMLProcessed.R
@@ -1,18 +1,26 @@
-readNMRMLProcessed <-
-function (filename) {
+#' readNMRMLProcessed
+#'
+#' Extract binary processed data from nmrML
+#'
+#' This is the Details section
+#'
+#' @param filename character Filename of the nmrML to check
+#' @return A vector with the numeric values of the processed data
+#' @author Steffen Neumann
+#' @examples
+#' length(readNMRMLProcessed(system.file("examples/HMDB00005.nmrML", package = "nmRIO")))
+#' @export
+
+readNMRMLProcessed <- function (filename) {
     tree <- xmlTreeParse(filename)
     root <- xmlRoot(tree)
-
+    
     ## Extract base64encoded data 
     b64s <- sapply (xmlElementsByTagName(root, "binary",
                                          recursive = TRUE), xmlValue)
 
     intensities <- base64decode(b64s[2], "double", size=8)
-    
-    ## gzfid <- base64decode(b64s[2], "raw")
-    ## rawfid <- memDecompress(gzfid, type="gzip")
-    ## fid <- readBin(rawfid, what="complex")
-    
+        
     ## Get required parameters from nmrML
     irradiationFrequency <- as.double(xmlAttrs(xmlElementsByTagName(root, "irradiationFrequency", recursive = TRUE)[[1]])["value"])
 
@@ -22,7 +30,6 @@ function (filename) {
 
     ## delayTime <- 1.0
     ## ppmOffset <- delayTime / 599.4094446,
-
     
     ppm <- seq(from=14.77180,
                to= -5.239921,
