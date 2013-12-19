6a324875af8f7fd0e0f2926bc63bef76dfec7d71
Michael Wilson <michael.wilson@ualberta.ca>
2013-12-18 18:51:22 -0700

    Fix adding annotation example

--- tools/Parser_and_Converters/python/pynmrml/examples/adding_annotation.py
+++ tools/Parser_and_Converters/python/pynmrml/examples/adding_annotation.py
@@ -1,7 +1,7 @@
 import sys, os, glob
 
 # add the converter tools to the syspath
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 
 from pynmrml import io
 from pynmrml import nmrML
